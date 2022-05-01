f0cf8e1a55e6ff7a608ce19c861430c70899ad16
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-05-01 11:33:59 +0300

    moved

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,5 @@
-const getAllContacts = require("./contacts/getAllContacts");
-const getContactById = require("./contacts/getContactsById");
-const addContact = require("./contacts/addContact");
-const removeContact = require("./contacts/removeContact");
-const updateContact = require("./contacts/updateContact");
-const updateStatusContact = require("./contacts/updateStatusContact");
+const contacts = require("./contacts");
 
 module.exports = {
-  getAllContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-  updateStatusContact,
+  contacts,
 };
