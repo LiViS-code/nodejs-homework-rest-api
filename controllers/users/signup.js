ef64e468407a646c593db14d82e327a4f32f4caa
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-05-04 08:08:20 +0300

    smaller fix

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -2,25 +2,29 @@ const { User } = require("../../models");
 const { createError } = require("../../helpers");
 
 const signup = async (req, res) => {
-  const { email: desiredEmail } = req.body;
+  const { email } = req.body;
 
-  const user = await User.findOne({ email: desiredEmail }).exec();
+  const user = await User.findOne({ email });
 
   if (user) {
     throw createError(
       409,
-      `A user with e-mail address <${desiredEmail}> is already registered.`
+      `a user with e-mail address "${email}" is already registered`
     );
   }
 
-  const { email, subscription } = await User.create(req.body);
+  // TODO захешировать пароль перед сохранением.
 
-  res.json({
+  const { subscription } = await User.create(req.body);
+
+  res.status(201).json({
     status: "success",
     code: 201,
     data: {
-      email,
-      subscription,
+      user: {
+        email,
+        subscription,
+      },
     },
   });
 };
