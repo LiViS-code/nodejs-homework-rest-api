607d041406e92118e761273ac041b21e76e2dc0b
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-05-04 08:06:21 +0300

    fix status message

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/users/signup", usersRouter);
+app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ status: 404, message: "Not found" });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
-  res.json({ status, message });
+  res.status(status).json({ status, message });
 });
 
 module.exports = app;
