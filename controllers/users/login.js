7cac1f47c47df49816e83addd3742d464296e39a
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-05-04 12:23:13 +0300

    install module jwt

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -2,21 +2,28 @@ const { User } = require("../../models");
 const { createError } = require("../../helpers");
 
 const login = async (req, res) => {
-  const { email: desiredEmail, password } = req.body;
+  const { email, password } = req.body;
   console.log("req.body", req.body);
-  const user = await User.findOne({ email: desiredEmail });
+  const user = await User.findOne({ email });
 
-  if (!user || user.password !== password) {
+  if (!user || !user.comparePassword(password)) {
     throw createError(401, `Email or password is wrong`);
   }
 
   // TODO выдать токен
+  user.token = "example token";
+
+  user.save();
 
   res.status(200).json({
     status: "success",
     code: 200,
     data: {
-      user,
+      token: user.token,
+      user: {
+        email,
+        subscription: user.subscription,
+      },
     },
   });
 };
