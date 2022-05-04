547caf974774791a1ea3c0468d9c101fc3c81564
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-05-04 08:08:50 +0300

    upd message

--- helpers/createError.js
+++ helpers/createError.js
@@ -1,11 +1,18 @@
 const statusMessages = {
   400: "Bad request",
+  401: "Unauthorized",
   404: "Not found",
   409: "Conflict",
   500: "Server error",
 };
 
-const createError = (status, message = statusMessages[status]) => {
+const createError = (status, message) => {
+  if (message) {
+    message = `${statusMessages[status]}: ${message}`;
+  } else {
+    message = statusMessages[status];
+  }
+
   const error = new Error(message);
   error.status = status;
   return error;
