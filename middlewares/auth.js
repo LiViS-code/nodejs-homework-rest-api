49c47e0d6268c28750fdd2b1a46130d87b87cd05
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-05-05 13:20:32 +0300

    add controller auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,26 +9,32 @@ const auth = async (req, res, next) => {
 
   try {
     if (!authorization) {
-      throw createError(401, "Not authorized 1");
+      throw createError(401, "Not authorized");
     }
 
     const [bearer, token] = authorization.split(" ");
 
     if (bearer !== "Bearer") {
-      throw createError(401, "Not authorized 2");
+      throw createError(401, "Not authorized");
     }
+
     const { id } = jwt.verify(token, SECRET_KEY);
 
     const user = await User.findById(id);
 
-    if (!user || !user.token) {
-      throw createError(401, "Not authorized 3");
+    if (!user) {
+      throw createError(401, "Not authorized");
     }
 
     req.user = user;
 
     next();
   } catch (error) {
+    if (error.message.toLowerCase() === "invalid signature") {
+      error.status = 401;
+      error.message = "Unauthorized: invalid signature";
+    }
+
     next(error);
   }
 };
