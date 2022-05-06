fc6271c0106e65611e5ce807c0e69d09cf393b84
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-05-06 07:11:22 +0300

    add user id to output info

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,5 +1,5 @@
 const getCurrent = async (req, res) => {
-  const { email, subscription } = req.user;
+  const { email, subscription, _id: userId } = req.user;
   res.status(200).json({
     status: "success",
     code: 200,
@@ -7,6 +7,7 @@ const getCurrent = async (req, res) => {
       user: {
         email,
         subscription,
+        userId,
       },
     },
   });
