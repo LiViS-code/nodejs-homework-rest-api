36c00cd986d4f6fc37ed4cffc798cf4e0513a2a7
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-05-09 18:40:11 +0300

    small fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,16 +8,16 @@ const auth = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   try {
     if (!authorization) {
-      throw createError(401, "Not authorized");
+      throw createError(401, "not authorized");
     }
     const [bearer, token] = authorization.split(" ");
     if (bearer !== "Bearer") {
-      throw createError(401, "Not authorized");
+      throw createError(401, "not authorized");
     }
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token) {
-      throw createError(401, "Not authorized");
+      throw createError(401, "not authorized");
     }
     req.user = user;
     next();
